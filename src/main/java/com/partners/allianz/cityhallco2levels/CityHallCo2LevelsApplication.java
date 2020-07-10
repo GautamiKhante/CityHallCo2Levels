b51b3053aac130a1288747e3ce4d4298b1c66efc
GautamiKhante <gautami004.khante@gmail.com>
2020-07-10 18:42:57 +0530

    Initial commmit.

---
+++ src/main/java/com/partners/allianz/cityhallco2levels/CityHallCo2LevelsApplication.java
@@ -0,0 +1,13 @@
+package com.partners.allianz.cityhallco2levels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityHallCo2LevelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityHallCo2LevelsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/partners/allianz/cityhallco2levels/CityHallCo2LevelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partners.allianz.cityhallco2levels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityHallCo2LevelsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
